44802ec1630a673de8989039bb553bf48ac0379b
patrick reiter <patrick.reiter@itsv.at>
2026-02-17 10:54:28 +0100

    EXTEND: main.go: Interface-Beispiel erweitert um Saveable-Interface

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,6 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// Saveable interface defines types that can be saved to a file
+type Saveable interface {
+	Save() string
+}
+
+type User struct {
+	Name string
+}
+
+type Product struct {
+	Name  string
+	Price float64
+}
+
+func (u User) Save() string {
+	return fmt.Sprintf("Saving user: %s", u.Name)
+}
+
+func (p Product) Save() string {
+	return fmt.Sprintf("Saving product: %s", p.Name)
+}
+
+// Generic Save function that works with any Saveable type
+func SaveData(data Saveable) error {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	filename := fmt.Sprintf("%T.json", data) // Use the type name as the filename
+	return os.WriteFile(filename, jsonData, 0644)
+}
 
 func main() {
 	// type assertions
@@ -22,5 +59,18 @@ func main() {
 		fmt.Printf("bool: %v\n", a)
 	default:
 		fmt.Print("Unknown Type\n")
+	}
+
+	user := User{Name: "John"}
+	product := Product{Name: "Laptop", Price: 999.99}
+
+	err := SaveData(user)
+	if err != nil {
+		fmt.Println("Error saving user:", err)
+	}
+
+	err = SaveData(product)
+	if err != nil {
+		fmt.Println("Error saving product:", err)
 	}
 }
